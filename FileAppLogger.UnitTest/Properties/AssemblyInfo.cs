b1c7e8e55a6aacc05441127064234863bfb366ce
Jitendra <jitendra.panchal25@gmail.com>
2022-07-07 16:34:04 +0530

    Initial Commit - Committing FileAppLogger solution

---
+++ FileAppLogger.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FileAppLogger.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FileAppLogger.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4b4d1c4a-04f3-41a5-9ff0-a41f5e6e86a1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FileAppLogger/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAppLogger
+{
+    public class Logger
+    {
+        private static string _logFilePath = @"D:\TempLog\MyFileAppLogs.log";
+
+        public static void LogToFile(string message)
+        {
+            try
+            {
+                File.AppendAllText(_logFilePath, $"{DateTime.Now.ToShortDateString()}--{DateTime.Now.ToShortTimeString()}--\t {message} \n\n");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
